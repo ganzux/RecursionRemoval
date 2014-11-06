a9daab7ffe72e689568ce697bcc14d46c602bebe
Álvaro <ganzux@users.noreply.github.com>
2014-11-06 16:08:17 +0100

    Update Recursion.java
    Not main perpendicular and possibility to set the inverse

--- src/Recursion.java
+++ src/Recursion.java
@@ -1,108 +1,111 @@
 
-public class Recursion {
-
+public class Recursive {
 	public static void main( String[] args ) throws Exception {
 
 		int times = 14;
 
-		for (int i = 0; i <= times; i++) {
-			for (int j = 0; j <= times; j++) {
-				int a = f(i, j);
-				int b = f2(i, j);
-				int c = f3(i, j);
-				if (a != b || a != c)
-					throw new Exception("Error en el programa: " + a + ", " + b + ", " + c);
-				else
-					System.out.println("OK para " + i + ", " + j + ": " + a );
-			}
-		}
-
+    	for (int i = 0; i <= times; i++) {
+    		for (int j = 0; j <= times; j++) {
+    			int a = f(i, j);
+    			int b = f2(i, j);
+    			int c = f3(i, j);
+    			
+    			if (a != b || a != c)
+    				throw new Exception("Error en el programa");
+    			else
+    				System.out.println("OK para " + i + ", " + j + ": " + a );
+    		}
+    	}
+    	
 		// BASE CASE
-		long init = System.currentTimeMillis();
+    	long init = System.currentTimeMillis();
 
-		for (int i = 0; i <= times; i++)
-			for (int j = 0; j <= times; j++)
-				f(i, j);
+    	for (int i = 0; i <= times; i++)
+    		for (int j = 0; j <= times; j++)
+    			f(i, j);
 
-		long end = System.currentTimeMillis();
+    	long end = System.currentTimeMillis();
+    	
+    	System.out.println("Tiempo para funcion inicial: " + (end-init) );
+    	// END OF BASE CASE
+    	
+    	// MULTIPLE RECURSIVITY REMOVED BY MEMORY
+    	init = System.currentTimeMillis();
 
-		System.out.println("Tiempo para funcion inicial: " + (end-init) );
-		// END OF BASE CASE
+    	for (int i = 0; i <= times; i++)
+    		for (int j = 0; j <= times; j++)
+    			f2(i, j);
 
-		// MULTIPLE RECURSIVITY REMOVED BY MEMORY
-		init = System.currentTimeMillis();
+    	end = System.currentTimeMillis();
+    	
+    	System.out.println("Tiempo con eliminacion de la recursividad multiple por memorizacion: " + (end-init) );
+    	// END OF MULTIPLE RECURSIVITY REMOVED BY MEMORY
 
-		for (int i = 0; i <= times; i++)
-			for (int j = 0; j <= times; j++)
-				f2(i, j);
+    	// MULTIPLE RECURSIVITY REMOVED BY TABULATION
+    	init = System.currentTimeMillis();
 
-		end = System.currentTimeMillis();
+    	for (int i = 0; i <= times; i++)
+    		for (int j = 0; j <= times; j++)
+    			f3(i, j);
 
-		System.out.println("Tiempo con eliminacion de la recursividad multiple por memorizacion: " + (end-init) );
-		// END OF MULTIPLE RECURSIVITY REMOVED BY MEMORY
+    	end = System.currentTimeMillis();
+    	
+    	System.out.println("Tiempo con eliminacion de la recursividad multiple por tabulacion: " + (end-init) );
+    	// END OF MULTIPLE RECURSIVITY REMOVED BY TABULATION
+    }
+    
+    private static int f(int x, int y){
+    	if (x == 0)
+    		return y;
+    	else if (y == 0)
+    		return x;
+    	else 
+    		return f(x, y-1) + f(x-1, y) + f(x-1, y-1);
+    }
+    
+    private static int f2(int x, int y){
 
-		// MULTIPLE RECURSIVITY REMOVED BY TABULATION
-		init = System.currentTimeMillis();
+    	int [][] combs = new int[x+1][y+1];
+    	for ( int i = 0; i < x; i++)
+    		for ( int j = 0; j < y; j++)
+    			combs[i][j] = 0;
 
-		for (int i = 0; i <= times; i++)
-			for (int j = 0; j <= times; j++)
-				f3(i, j);
+    	fMem(x, y, combs);
+ 
+    	return combs[x][y];
+    }
+    
+    private static void fMem(int x, int y, int [][] combs) {
+    	
+    	// If the solution hasnt been calculated
+    	if(combs[x][y] == 0){
+    		
+    		// 1st base case
+	    	if (x == 0) {
+	    		combs[x][y] =  y;
+	    	}
+	    	
+	    	// 2nd base case
+	    	else if (y == 0) {
+	    		combs[x][y] =  x;
+	    	}
 
-		end = System.currentTimeMillis();
-
-		System.out.println("Tiempo con eliminacion de la recursividad multiple por tabulacion: " + (end-init) );
-		// END OF MULTIPLE RECURSIVITY REMOVED BY TABULATION
-	}
-
-	private static int f(int x, int y){
-		if (x == 0)
-			return y;
-		else if (y == 0)
-			return x;
-		else 
-			return f(x, y-1) + f(x-1, y) + f(x-1, y-1);
-	}
-
-	private static int f2(int x, int y){
-
-		int [][] combs = new int[x+1][y+1];
-		for ( int i = 0; i < x; i++)
-			for ( int j = 0; j < y; j++)
-				combs[i][j] = 0;
-
-		fMem(x, y, combs);
-
-		return combs[x][y];
-	}
-
-	private static void fMem(int x, int y, int [][] combs) {
-
-		// If the solution hasnt been calculated
-		if(combs[x][y] == 0){
-
-			// 1st base case
-			if (x == 0) {
-				combs[x][y] =  y;
-			}
-
-			// 2nd base case
-			else if (y == 0) {
-				combs[x][y] =  x;
-			}
-
-			else{
-				// Recursion calls
-				fMem(x, y-1,	combs);
-				fMem(x-1, y, 	combs);
-				fMem(x-1, y-1,	combs);
-				// and the calc of the cell
-				combs[x][y] = combs[x][y-1] + combs[x-1][y] + combs[x-1][y-1];
-			}
-		}
-	}
-
-
-	private static int f3(int x, int y){
+	    	else{
+	    		// Recursion calls
+	    		fMem(x, y-1,	combs);
+	    		fMem(x-1, y, 	combs);
+	    		fMem(x-1, y-1,	combs);
+	    		// and the calc of the cell
+	    		combs[x][y] = combs[x][y-1] + combs[x-1][y] + combs[x-1][y-1];
+	    		// Not main perpendicular and possibility to set the inverse
+	    		if (x != y && y<combs.length && x<combs[x].length)
+	    			combs[y][x] = combs[x][y];
+	    	}
+    	}
+    }
+    
+    
+    private static int f3(int x, int y){
 
 		int [][] combs = new int[x + 1][y + 1];
 
@@ -116,9 +119,14 @@ public class Recursion {
 
 		// Calc of the rest of values according with the base cases.
 		// Very important the beginning of the aux variables i and j!
-		for (int i = 1;i <= x;i++)
-			for (int j = 1;j <= y;j++)
+		for (int i = 1;i <= x;i++) {
+			for (int j = 1;j <= y;j++) {
 				combs[i][j] = combs[i][j-1] + combs[i-1][j] + combs[i-1][j-1];
+				// Not main perpendicular and possibility to set the inverse
+	    		if (x != y && y<combs.length && x<combs[x].length)
+	    			combs[y][x] = combs[x][y];
+			}
+		}
 
 		return combs[x][y];
 	}
